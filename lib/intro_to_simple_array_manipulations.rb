417dc9fad5ba916eff0d6af97dc307aad011f8fa
Jurabek Tolmasov <tolmasovjurabek@gmail.com>
2020-01-01 09:18:32 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -10,10 +10,8 @@ end
     continents.pop
   end
   
-describe "pop_with_args" do
-  before(:each) do 
-    @dog_breeds = ["Great Dane", "Golden Retriever", "Chihuahua", "Shiba Inu"]
-    @small_dogs = pop_with_args(@dog_breeds)
+    dog_breeds = ["Great Dane", "Golden Retriever", "Chihuahua", "Shiba Inu"]
+    small_dogs = pop_with_args(@dog_breeds)
   end 
   it "takes in an argument of an array and uses the pop method with an argument of 2 to remove the last two array items and return them" do 
     expect(@small_dogs).to eq(["Chihuahua", "Shiba Inu"])
